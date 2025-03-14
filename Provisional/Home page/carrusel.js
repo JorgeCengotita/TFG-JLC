a64e0e87ba8ec224d14808187a90f8999d8ac28c
JorgeCengotita <jluis-cengotita1@eep-igroup.com>
2025-03-14 19:38:32 +0100

    Cards home page v1

--- Provisional/Home page/carrusel.js
+++ Provisional/Home page/carrusel.js
@@ -1,21 +1,21 @@
 // Inicializar Swiper
 var swiper = new Swiper('#newsliderid', {
-    slidesPerView: 3,       // Mostrar 3 diapositivas a la vez
-    spaceBetween: 0,        // Espacio entre las diapositivas (ajústalo si es necesario)
-    loop: true,              // Habilitar el bucle
-    centeredSlides: true,    // Asegurarnos de que la diapositiva activa esté siempre en el centro
-    navigation: {            // Botones de navegación
+    slidesPerView: 3,
+    spaceBetween: 0,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     breakpoints: {
       768: {
-        slidesPerView: 3,    // En pantallas grandes mostrar 3 diapositivas
+        slidesPerView: 3,
       },
       480: {
-        slidesPerView: 1,    // En pantallas pequeñas mostrar 1 diapositiva
+        slidesPerView: 1,
       }
     }
-  });
+});
   
   
